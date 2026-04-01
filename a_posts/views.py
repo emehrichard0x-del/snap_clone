af698d98d095a68d4b4f1d568c75e20b951504a8
emehrichard0x-del <emehrichard0x@gmail.com>
2026-04-01 02:21:21 -0700

    Fix line endings

---
+++ a_posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def home_views(request):
+     posts = Post.objects.all()
+     return render(request, 'a_posts/home.html',{'posts' : posts})
+
+
+def post_create_view(request):
+    return render(request, 'a_posts/post_create.html')
